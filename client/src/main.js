c7f9aee3812e8fd441f31ce32e79430fc0b74832
Max Bernard <max@tower-1.homebase.net>
2018-08-12 18:06:54 -0400

    Initial Vue setup

--- client/src/main.js
+++ client/src/main.js
@@ -1,10 +1,22 @@
+// 'use strict'
+
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
-import App from './App.vue';
+import { sync } from 'vuex-router-sync';
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
+// import Panel from './components/Panel';
+import store from './store/index';
+import App from './App';
 import router from './router';
-import store from './store';
 
 Vue.config.productionTip = false;
 
+Vue.use(Vuetify);
+sync(store, router);
+
+/* eslint-disable no-new */
 new Vue({
   router,
   store,
---
+++ client/src/router/index.js
@@ -0,0 +1,27 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import Projects from '../views/Projects';
+import Login from '../views/Login';
+import Register from '../views/Register';
+
+Vue.use(Router);
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'projects',
+      component: Projects,
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login,
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: Register,
+    },
+  ],
+});
